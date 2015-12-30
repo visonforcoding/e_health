6c451db7bbdb937a605766c5fa874ee7321e67cf
caowenpeng <caowenpeng@061265c2-d7df-45f2-a718-26038c900575>
2015-12-30 12:23:27 +0000

    微信支付

--- application/controllers/home/pay.php
+++ application/controllers/home/pay.php
@@ -166,8 +166,8 @@ class Pay extends Home_Controller {
         $input->SetTrade_type("JSAPI");
         $input->SetOpenid($openId);
         $order = WxPayApi::unifiedOrder($input);
+        var_dump($order);
         echo '<font color="#f00"><b>统一下单支付单信息</b></font><br/>';
-        printf_info($order);
     }
 
     public function wxorderNotify() {
