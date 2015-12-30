573ecf7279bfcb9ccc4a995306ee1374e5482750
caowenpeng <caowenpeng1990@126.com>
2015-12-30 12:41:01 +0000

    微信支付

--- application/controllers/home/pay.php
+++ application/controllers/home/pay.php
@@ -166,9 +166,9 @@ class Pay extends Home_Controller {
         $input->SetTrade_type("JSAPI");
         $input->SetOpenid($openId);
         $order = WxPayApi::unifiedOrder($input);
-        var_dump($order);
+//        var_dump($order);
         echo '<font color="#f00"><b>统一下单支付单信息</b></font><br/>';
-        printf_info($order);
+//        printf_info($order);
         $tools = new JsApiPay();
         $jsApiParameters = $tools->GetJsApiParameters($order);
         $this->twig->render('home/pay/pay.twig', array(
